5ed58a909951c8a8548c1e55a45c5f0e1f327ebf
ggdropkal <44447172+Kotyarishka@users.noreply.github.com>
2024-05-12 20:50:17 +0300

    init

--- main.cpp
+++ main.cpp
@@ -1,25 +1,49 @@
+#include <string>
 #include <iostream>
 
-void increment(int &x) {
-    x++;
-}
-
-void increment(int &x, int amount) {
-    x += amount;
-}
-
 int main() {
-    int var = 0;
+    std::string values;
+    std::getline(std::cin, values);
 
-    for (int i = 0; i < 10; i++) {
-        if (i % 2) {
-            increment(var);
-        } else {
-            increment(var, i);
+    std::string templateString;
+    std::getline(std::cin, templateString);
+
+    // Put values into template
+    // values format: "person=Paul,action=strolls around,place=the park"
+    // template format: "[person] [action] [place]"
+    // result: "Paul strolls around the park"
+    std::string result;
+
+    // split values into pairs
+    std::string delimiter = ",";
+    size_t pos = 0;
+
+    while ((pos = values.find(delimiter)) != std::string::npos) {
+        std::string pair = values.substr(0, pos);
+        size_t equalPos = pair.find("=");
+        std::string key = pair.substr(0, equalPos);
+        std::string value = pair.substr(equalPos + 1);
+        values.erase(0, pos + delimiter.length());
+
+        // replace key with value in template
+        size_t keyPos = templateString.find("[" + key + "]");
+        if (keyPos != std::string::npos) {
+            templateString.replace(keyPos, key.length() + 2, value);
         }
     }
 
-    std::cout << var << std::endl;
+    // last pair
+    size_t equalPos = values.find("=");
+    std::string key = values.substr(0, equalPos);
+    std::string value = values.substr(equalPos + 1);
+
+    // replace key with value in template
+    size_t keyPos = templateString.find("[" + key + "]");
+    if (keyPos != std::string::npos) {
+        templateString.replace(keyPos, key.length() + 2, value);
+    }
+
+    std::cout << templateString << std::endl;
 
     return 0;
 }
